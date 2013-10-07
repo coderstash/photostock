c11b49d806400bad8586bca21f31bd6efefa2fc8
Bob Hanson <bob@cube2media.com>
2013-10-07 11:03:32 -0600

    added nTimes

--- app/assets/javascripts/helpers/format.js
+++ app/assets/javascripts/helpers/format.js
@@ -1,3 +1,10 @@
+Handlebars.registerHelper('times', function(n, block) {
+    var accum = '';
+    for(var i = 0; i < n; ++i)
+        accum += block.fn(i);
+    return accum;
+});
+
 Ember.Handlebars.helper('upcase', function(textValue) {
 	return textValue.toUpperCase();
 });
@@ -19,7 +26,7 @@ Ember.Handlebars.helper('formatFileSize', function(fileSize) {
 Ember.Handlebars.helper('titleize', function(textValue) {
 	var splitString = textValue.split(' '),
 			segment;
-  
+
   for ( var i = 0; i < splitString.length; i++ ) {
   	segment = splitString[i].charAt(0).toUpperCase();
         splitString[i] = segment + splitString[i].substr(1);
